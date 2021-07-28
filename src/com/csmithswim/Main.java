75c05f4cc4c61f6fec31fc1105d1c9b685117cb5
Cullen S <cullensmi@gmail.com>
2021-07-28 12:50:19 -0400

    solved tic tac toe challenege

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -1,11 +1,12 @@
 package com.csmithswim;
-import org.w3c.dom.ls.LSOutput;
+import java.util.Scanner;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+//        String input = scanner.nextLine();
+        String input = "         ";
         char[][] twoDArray = new char[3][3];
         int counter = 0;
         for (int i = 0; i < 3; i++) {
@@ -14,6 +15,7 @@ public class Main {
                 counter++;
             }
         }
+
 
         System.out.println("---------");
         for (int i = 0; i < input.length() - 1; i += 3) {
@@ -24,21 +26,40 @@ public class Main {
         boolean flag;
         int row;
         int column;
+        boolean game = true;
 
-        do {
-            System.out.print("Enter the coordinates: ");
+
+        int x = 0;
+        int o = 0;
+        int spaces = 9;
+        int xWins = 0;
+        int oWins = 0;
+        boolean xTurn = true;
+
+        while (game) {
+            System.out.print("Enter the coordinates:");
             Scanner selection = new Scanner(System.in);
 
-        try {
-            row = selection.nextInt();
-            column = selection.nextInt();
-            if (twoDArray[row-1][column-1] != '_') {
-                System.out.println("Cell is occupied!");
-                flag = true;
-                continue;
-            }
-            twoDArray[row-1][column-1] = 'X';
-            flag = false;
+            try {
+                row = selection.nextInt();
+                column = selection.nextInt();
+                if (twoDArray[row - 1][column - 1] != ' ') {
+                    System.out.println("Cell is occupied!");
+                    flag = true;
+                    continue;
+                }
+                if (xTurn) {
+                    twoDArray[row - 1][column - 1] = 'X';
+                    spaces--;
+                    flag = false;
+                    xTurn = false;
+                } else {
+                    twoDArray[row - 1][column - 1] = 'O';
+                    spaces--;
+                    xTurn = true;
+                }
+
+
             } catch (InputMismatchException e) {
                 System.out.println("You should enter numbers!");
                 flag = true;
@@ -46,59 +67,144 @@ public class Main {
                 System.out.println("Coordinates should be from 1 to 3!");
                 flag = true;
             }
-        } while (flag);
-
-        System.out.println("---------");
-        for (int i = 0; i < 3; i++) {
-            String output = "";
-            for (int j = 0; j < 3; j++){
-                output += twoDArray[i][j] + " ";
+            System.out.println("---------");
+            for (int i = 0; i < 3; i++) {
+                String output = "";
+                for (int j = 0; j < 3; j++) {
+                    output += twoDArray[i][j] + " ";
+                }
+                System.out.println("| " + output + "|");
             }
-            System.out.println("| " + output + "|");
-        }
-        System.out.println("---------");
+            System.out.println("---------");
 
+            if (twoDArray[0][0] + twoDArray[1][1] + twoDArray[2][2] == 264) {
+                xWins++;
+            }
+            if (twoDArray[0][0] + twoDArray[1][1] + twoDArray[2][2] == 237) {
+                oWins++;
+            }
+            if (twoDArray[0][2] + twoDArray[1][1] + twoDArray[2][0] == 264){
+                xWins++;
+            }
+            if (twoDArray[0][2] + twoDArray[1][1] + twoDArray[2][0] == 237){
+                oWins++;
+            }
+            if (spaces == 0) {
+                System.out.println("Draw");
+                game = false;
+                break;
+            }
+            else if (xWins == 1 && oWins == 0) {
+                System.out.println("X wins");
+                game = false;
+            } else if (oWins == 1 && xWins == 0) {
+                System.out.println("O wins");
+                game = false;
+            }
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (twoDArray[i][j] == 'X') {
+                        x++;
+                    }
+                    if (twoDArray[i][j] == 'O') {
+                        o++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 1; j++) {
+                    //Rows
+                    if (twoDArray[i][j] + twoDArray[i][j + 1] + twoDArray[i][j + 2] == 264) {
+                        xWins++;
+                        break;
+                    }
+                    if (twoDArray[i][j] + twoDArray[i][j + 1] + twoDArray[i][j + 2] == 237) {
+                        oWins++;
+                        break;
+                    }
+                    //Columns
+                    if (twoDArray[j][i] + twoDArray[j + 1][i] + twoDArray[j + 2][i] == 264) {
+                        xWins++;
+                        break;
+                    }
+                    if (twoDArray[j][i] + twoDArray[j + 1][i] + twoDArray[j + 2][i] == 237) {
+                        oWins++;
+                        break;
+                    }
+                }
+            }
+
+
+//            else if (xWins < 1 && oWins < 1 & spaces > 0 && Math.abs(x-o) <= 1) {
+//                System.out.println("Game not finished");
+//            }
+
+//                    else if (x + o == 9) {
+//                        System.out.println("Draw");
+//                        game = false;
+//                        break;
+//                    }
+
+//            else if (xWins >= 1 && oWins >= 1 || Math.abs(x-o) > 1) {
+//                System.out.println("Impossible");
+//            }
+
+
+
+
+
+
+
+
+            if (spaces == 0) {
+                for (int i = 0; i < 3; i++) {
+                    String output = "";
+                    for (int j = 0; j < 3; j++) {
+                        output += twoDArray[i][j] + " ";
+                    }
+                    System.out.println("| " + output + "|");
+                }
+                System.out.println("---------");
+                System.out.println("Draw");
+                game = false;
+                break;
+            }
+            else if (xWins == 1 && oWins == 0) {
+                for (int i = 0; i < 3; i++) {
+                    String output = "";
+                    for (int j = 0; j < 3; j++) {
+                        output += twoDArray[i][j] + " ";
+                    }
+                    System.out.println("| " + output + "|");
+                }
+                System.out.println("---------");
+                System.out.println("X wins");
+                game = false;
+                break;
+            } else if (oWins == 1 && xWins == 0) {
+                for (int i = 0; i < 3; i++) {
+                    String output = "";
+                    for (int j = 0; j < 3; j++) {
+                        output += twoDArray[i][j] + " ";
+                    }
+                    System.out.println("| " + output + "|");
+                }
+                System.out.println("---------");
+                System.out.println("O wins");
+                game = false;
+                break;
+            }
+        }
     }
 }
 
-/*
-                    System.out.println("This cell is occupied! Choose another one!");
-                    System.out.println("Enter the coordinates: ");
-                    int row1 = scanner.nextInt();
-                    int column1 = scanner.nextInt();
-                    twoDArray[row1-1][column1-1] = 'X';
-                }
-
-        else if (twoDArray[row-1][column-1] == '_') {
-            twoDArray[row-1][column-1] = 'X';
-             */
-
-
-
-
-
-
-
-
-//From 3/5 challenge
-/*
- int x = 0;
+    /*
+        int x = 0;
         int o = 0;
         int spaces = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (twoDArray[i][j] == 'X') {
-                    x++;
-                }
-                if (twoDArray[i][j] == 'O') {
-                    o++;
-                }
-                if (twoDArray[i][j] == '_'){
-                    spaces++;
-                }
-            }
-        }
- int xWins = 0;
+        int xWins = 0;
         int oWins = 0;
         if (twoDArray[0][0] + twoDArray[1][1] + twoDArray[2][2] == 264) {
             xWins++;
@@ -112,7 +218,18 @@ public class Main {
         if (twoDArray[0][2] + twoDArray[1][1] + twoDArray[2][0] == 237){
             oWins++;
         }
-for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (twoDArray[i][j] == 'X') {
+                    x++;
+                }
+                if (twoDArray[i][j] == 'O') {
+                    o++;
+                }
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 1; j++) {
                 //Rows
                 if (twoDArray[i][j] + twoDArray[i][j+1] + twoDArray[i][j+2] == 264) {
@@ -132,11 +249,14 @@ for (int i = 0; i < 3; i++) {
                     oWins++;
                     break;
                 }
+                if (twoDArray[i][j] == '_'){
+                    spaces++;
+                }
             }
         }
 
-        if (xWins == 1 && oWins == 0) {
-            System.out.println("X wins");
+              if (xWins == 1 && oWins == 0) {
+        System.out.println("X wins");
         }
         else if (oWins == 1 && xWins == 0) {
             System.out.println("O wins");
@@ -150,4 +270,4 @@ for (int i = 0; i < 3; i++) {
         else if (xWins >= 1 && oWins >= 1 || Math.abs(x-o) > 1) {
             System.out.println("Impossible");
         }
- */
+    */
